87d893a132ee7f7c7b715bbf2373580b8ef9ab10
Stmrspo <benjaminrcleveland@gmail.com>
2024-09-18 11:15:06 -0700

    Added function to play a single round, and scorekeeping variables

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3); 
     if (x === 0)
-        return "Rock";
+        return "rock";
     else if (x === 1)
-        return "Paper";
+        return "paper";
     else   
-        return "Scissors";
+        return "scissors";
 }
-console.log(getComputerChoice()); 
+
+
+
+function getHumanChoice() {
+   let answer = (prompt("Rock, Paper, or Scissors?")).toLowerCase();   
+            
+    return answer;
+}
+
+computerScore = 0;
+humanScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+        return;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("paper beats rock, a point for the computer.");
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("Rock beats scissors; a point for you!");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("Paper beats rock; a point for you!");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("Scissors beats paper; a point for the computer.");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("Rock beats scissors; a point for the computer.");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Scissors beats paper; a point for you!");
+        humanScore++;
+    }
+  
+}
+playRound();
+console.log(humanScore);
+console.log(computerScore);
